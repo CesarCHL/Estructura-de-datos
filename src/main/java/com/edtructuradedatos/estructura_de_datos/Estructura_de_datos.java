72aff420bb54a3ad83745f3a3ef9c2d676c1477a
CesarCHL <cesarchavira314@gmail.com>
2025-02-06 10:46:07 -0600

    upload project

---
+++ src/main/java/com/edtructuradedatos/estructura_de_datos/Estructura_de_datos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edtructuradedatos.estructura_de_datos;
+
+
+/**
+ *
+ * @author cesar
+ */
+public class Estructura_de_datos {
+
+    public static void main(String[] args) {
+        UserData objUserData = new UserData();
+        String fullName = objUserData.fullName("Cesar", "Chavira");
+        System.out.println(fullName);
+    }
+}
---
+++ src/main/java/com/edtructuradedatos/estructura_de_datos/UserData.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.edtructuradedatos.estructura_de_datos;
+
+/**
+ *
+ * @author cesar
+ */
+public class UserData {
+    public String fullName(String firstName, String lastName) {
+        return String.format("%s %s", firstName, lastName);
+    }
+}
